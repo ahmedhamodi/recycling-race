80b4c63552285034ce2bdd7b20854684e2065ea7
Prannoy Lal <prannoylal@Prannoys-MacBook-Pro.local>
2020-09-13 17:40:53 -0400

    fixed backend

--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,15 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import request
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+ 
+import os
 
 from .db import client as db_client
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+db = SQLAlchemy(app)
 
 blueprint = Blueprint('api', __name__)
 
@@ -10,3 +18,8 @@ blueprint = Blueprint('api', __name__)
 def get_recycling_data():
     data = [row.to_dict() for row in db_client.get_all_recycling_data()]
     return jsonify(data)
+
+@blueprint.route('/add-recycling-data', methods = ["POST"])
+def add_recycling_data():
+    db.session.add(request.data)
+    db.session.commit()
--- src/components/Display.js
+++ src/components/Display.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import BasicTable from './BasicTable';
 import firebase from 'firebase';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import 'firebase/firestore';
 
 import './Display.scss';
@@ -13,7 +15,18 @@ class Display extends React.Component {
     super(props);
     this.state = {
       dataPostgres: [],
-      dataFirebase: {}
+      dataFirebase: {},
+      teacherName: '',
+      aluminum: 0,
+      batteries: 0,
+      bottles: 0,
+      cans: 0,
+      cardboard: 0,
+      computerParts: 0,
+      glass: 0,
+      metal: 0,
+      paper: 0,
+      wood: 0
     };
   }
 
@@ -25,6 +38,36 @@ class Display extends React.Component {
         this.props.loadData(json)
       });
   }
+
+  addData = async () => {
+    let body = {
+      teacherName: this.state.teacherName,
+      aluminum: parseInt(this.state.aluminum),
+      batteries: parseInt(this.state.batteries),
+      bottles: parseInt(this.state.bottles),
+      cans: parseInt(this.state.cans),
+      cardboard: parseInt(this.state.cardboard),
+      computerParts: parseInt(this.state.computerParts),
+      glass: parseInt(this.state.glass),
+      metal: parseInt(this.state.metal),
+      paper: parseInt(this.state.paper),
+      wood: parseInt(this.state.wood)
+    };
+    const response = await fetch('/add-recycling-data', 
+    {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(body)
+    });
+    return response.json();
+  };
 
   fetchDataFirebase = () => {
     const fdb = firebase.firestore();
@@ -78,11 +121,83 @@ class Display extends React.Component {
     return teacherName
   }
 
+  setTeacherName = (event) => {
+    this.setState({
+      teacherName: event.target.value
+    });
+  }
+  setAluminum = (event) => {
+    this.setState({
+      aluminum: parseInt(event.target.value)
+    });
+  }
+  setBatteries = (event) => {
+    this.setState({
+      batteries: parseInt(event.target.value)
+    });
+  }
+  setBottles = (event) => {
+    this.setState({
+      bottles: parseInt(event.target.value)
+    });
+  }
+  setCans = (event) => {
+    this.setState({
+      cans: parseInt(event.target.value)
+    });
+  }
+  setCardboard = (event) => {
+    this.setState({
+      cardboard: parseInt(event.target.value)
+    });
+  }
+  setComputerParts = (event) => {
+    this.setState({
+      computerParts: parseInt(event.target.value)
+    });
+  }
+  setGlass = (event) => {
+    this.setState({
+      glass: parseInt(event.target.value)
+    });
+  }
+  setMetal = (event) => {
+    this.setState({
+      metal: parseInt(event.target.value)
+    });
+  }
+  setPaper = (event) => {
+    this.setState({
+      paper: parseInt(event.target.value)
+    });
+  }
+  setWood = (event) => {
+    this.setState({
+      wood: parseInt(event.target.value)
+    });
+  }
+
   render() {
     return (
       <div className="display-container">
         <h2>Current Leader:</h2>
         <p>{this.getHighScore()}'s classroom is in the lead!</p>
+        <h2>Add a new entry!</h2>
+        <TextField id="outlined-basic" label="Teacher Name" variant="outlined" onChange={this.setTeacherName}/>
+        <TextField id="outlined-basic" label="Aluminum" variant="outlined" onChange={this.setAluminum}/>
+        <TextField id="outlined-basic" label="Batteries" variant="outlined" onChange={this.setBatteries}/>
+        <TextField id="outlined-basic" label="Bottles" variant="outlined" onChange={this.setBottles}/>
+        <TextField id="outlined-basic" label="Cans" variant="outlined" onChange={this.setCans}/>
+        <TextField id="outlined-basic" label="Cardboard" variant="outlined" onChange={this.setCardboard}/>
+        <TextField id="outlined-basic" label="Computer Parts" variant="outlined" onChange={this.setComputerParts}/>
+        <TextField id="outlined-basic" label="Glass" variant="outlined" onChange={this.setGlass}/>
+        <TextField id="outlined-basic" label="Metal" variant="outlined" onChange={this.setMetal}/>
+        <TextField id="outlined-basic" label="Paper" variant="outlined" onChange={this.setPaper}/>
+        <TextField id="outlined-basic" label="Wood" variant="outlined" onChange={this.setWood}/>
+        <br></br>
+        <br></br>
+        <Button variant="contained" color="primary" onClick={this.addData}>Submit</Button>
+        <h2>Current Leader: </h2>
         <h2>Local Data Handling</h2>
         <BasicTable data={this.state.dataPostgres} />
         <h2>Global Data Handling</h2>
